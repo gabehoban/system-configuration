512b102554aa816ebcee2568585716a93b1b9b16
gabehoban <hello@gabehoban.com>
2022-12-03 15:28:20 -0500

    INIT

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -2,6 +2,6 @@
 
 mkdir -vp $HOME/.config/{git,tmux,zsh}
 
-for dir in ~/.config/system-configuration/programs; do 
+for dir in ~/.config/system-configuration/programs/*; do 
     (cd "$dir" && bash default.dotfile); 
 done
